ef9d48b1fa1faa7ce7ea4e227c5340380d055942
Tianyi Ding <td324@cornell.edu>
2016-08-31 22:26:00 -0400

    Create max_depth.java

---
+++ 97.Maximum-Depth-of-Binary-Tree/max_depth.java
@@ -0,0 +1,13 @@
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxDepth(TreeNode root) {
+        // write your code here
+        if(root == null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
+}
